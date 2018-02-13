930f727120118d0b33ea0ff948ca35ee847f6c1f
Josh Dunn <joshdunn.uwo@gmail.com>
2018-02-12 21:55:56 -0500

    update gitignore

--- app/models/resume.rb
+++ app/models/resume.rb
@@ -1,5 +1,5 @@
 class Resume < ApplicationRecord
-  has_attached_file :pdf, url: "/resume/:id/:filename"
+  has_attached_file :pdf, url: "/resume/:filename"
   validates_attachment_content_type :pdf, content_type: /application\/pdf/
 
   belongs_to :user
