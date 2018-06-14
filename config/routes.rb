b6ded336a9255939411026d172889e70031599c9
Josh Dunn <joshdunn.uwo@gmail.com>
2018-06-13 23:08:37 -0400

    some linting in routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,11 +4,11 @@ Rails.application.routes.draw do
     get 'login', to: 'devise/sessions#new', as: 'new_user_session'
     post 'login', to: 'devise/sessions#create', as: 'user_session'
     delete 'logout', to: 'devise/sessions#destroy', as: 'destroy_user_session'
-  end 
+  end
 
   resources :articles
-  resources :screenshots, only: [:create, :destroy]
-  resources :resumes, only: [:new, :create, :destroy]
-  
-  root "articles#index"
+  resources :screenshots, only: %i[create destroy]
+  resources :resumes, only: %i[new create destroy]
+
+  root 'articles#index'
 end
